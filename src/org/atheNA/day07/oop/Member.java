c88ff20787a271e03448fd56e132b4d86c306b15
athena <athena@atheNA>
2024-05-27 22:15:28 +0900

    [ADD] 24/05/27 자바 7일차 클래스와 객체
    1. 클래스의 개념과 설계 실습
    2. 객체와 메모리 구조
     - 참조변수는 stack영역에, 객체는 heap영역에 동적할당
    3. 접근지정자(제한자)
     - public
     - private
     - default
     - protected
    4. static에 대한 이해
     - static은 공유된다. 공기와 같이, 같은 클래스로 만든 객체에 대해서..

---
+++ src/org/atheNA/day07/oop/Member.java
@@ -0,0 +1,24 @@
+package org.atheNA.day07.oop;
+
+public class Member {
+	// 멤변수(필드)
+	public String name;
+	char gender;
+	public int age;
+	String education;
+	long salary;
+	public String job;
+	String property;	// 집1채, 자동채2대 / 500000000
+	String phone;		// 01012342345, '1'
+	String address;
+	boolean divorceYN;	// Yes or No
+	String children;		// 1남 1여, 아들1, 딸1
+	
+	public Member() {} // 생성자
+	
+	// 멤버메소드(메소드)
+//	public void registerMember() {}
+	public void doHeart() {}
+	public void sendMessage() {}
+	public void doPromise() {}
+}
