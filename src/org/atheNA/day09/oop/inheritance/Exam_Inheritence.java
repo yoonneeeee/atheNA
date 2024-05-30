9de5b158142576d14ab5174aa6fbab54cba26839
athena <athena@atheNA>
2024-05-30 11:22:24 +0900

    [ADD] 24/05/29 자바 9일차 객체지향원칙 2, 상속
    1. 상속의 개념 및 목적 이해
    2. 상속을 사용하는 방법
     - extends 키워드 사용
    3. 접근지정자 protected에 대한 이해 및 실습

---
+++ src/org/atheNA/day09/oop/inheritance/Exam_Inheritence.java
@@ -0,0 +1,17 @@
+package org.atheNA.day09.oop.inheritance;
+
+import org.atheNA.day09.oop.inheritance.point.ColorPoint;
+
+public class Exam_Inheritence {
+
+	public static void main(String[] args) {
+		ColorPoint cp = new ColorPoint(10,23,"green");
+		cp.showColorPoint();
+		
+//		ColorPoint cp = new ColorPoint();
+//		cp.setX(5);
+//		cp.setY(7);
+//		cp.showPoint();
+	}
+
+}
